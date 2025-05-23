449c7eed8cf5f05cf12ef091c270fa3438751f2e
Mikayla Kosmala <53801463+mikayla-kosmala@users.noreply.github.com>
2025-05-23 02:59:28 -0700

    Editting .gitignore

---
+++ Python.py
@@ -0,0 +1,113 @@
+from docx import Document
+import string
+import re
+import pandas as pd
+
+translator = str.maketrans('', '', string.punctuation)
+# Load your resume
+doc = Document(r'C:\Users\mikay\OneDrive\Documents\Resume to Tableau\Running Resume.docx')
+
+# Print all the paragraphs
+#for para in doc.paragraphs:
+#  print(para.text)
+job = []
+job_info = []
+job_title = []
+company = []
+start_end_date = []
+job_desc = []
+job_achievements = []
+found_title = 0
+found_company = 0
+found_date = 0
+found_desc = 0
+found_achievements = 0
+desc_found =0
+df = pd.DataFrame(columns=["Title", "Company", "Desc", "Accomplishments", "Start Date", "End Date"])
+for para in doc.paragraphs[:50]:
+    print(f"Style: {para.style.name} | Text: {para.text}")
+    #print(para.text)
+    if desc_found:
+        desc = para.text
+        desc_found = 0
+    if "Bullet List" in para.style.name:
+        new_row = {"Title": title, "Company": company, "Desc": desc, "Accomplishments":para.text, "Start Date":start, "End Date":end}
+        # Append the row
+        df = pd.concat([df, pd.DataFrame([new_row])], ignore_index=True)
+
+    for run in para.runs:
+        if run.bold:
+            text = run.text.strip()
+            if text:  # Ignore empty strings
+                title = text
+                list = re.sub(r'\t+','|',para.text[len(text)+2:]).split('|')
+                company=list[0]
+                start,end=list[1].split('–')
+                desc_found = 1
+                break
+
+print(df.head(50))
+
+df.to_excel(r"C:\Users\mikay\OneDrive\Documents\Resume to Tableau\resume_data.xlsx", index=False)
+#Second Attempt
+# for para in doc.paragraphs:
+#     print(f"Style: {para.style.name} | Text: {para.text}")
+#     #print(para.text)
+#     if "Bullet List" in para.style.name:
+        
+#     for run in para.runs:
+#         if run.bold:
+#             text = run.text.strip()
+#             if text:  # Ignore empty strings
+#                 job_title.append(text)
+                
+#                 list = re.sub(r'\t+','|',para.text[len(text)+2:]).split('|')
+#                 print(list)
+#                 company.append(list[0])
+#                 start_end_date.append(list[1])
+#                 break
+       
+
+
+
+#print(job_title)
+#print (company)
+
+#FIRST ATTEMPT
+# for para in doc.paragraphs:
+#     print("•", para.text)
+#     for run in para.runs:
+#         text = run.text.strip()
+#         #print(text)
+#         if 'List Bullet' in para.style.name:
+#             print("•", para.text.strip())
+#         if found_date:
+#             if text:
+#                 job_desc.append(text)
+#                 found_date = 0
+#         if found_company:
+#             if text:
+#                 start_end_date.append(text)
+#                 found_company = 0
+#                 found_date = 1
+#         if found_title:
+#             if text:
+#                 company.append(text.translate(translator)[1:])
+#             found_title = 0
+#             found_company = 1
+#         if run.bold:
+#             if text:  # Ignore empty strings
+#                 job_title.append(text)
+#                 found_title = 1
+
+
+#print(job_desc)   
+
+
+    
+# for para in doc.paragraphs:
+#     if "Heading" in para.style.name:
+#         print(">> Section:", para.text)
+
+
+        
